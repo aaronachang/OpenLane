a5fe2e0e949f6d18da06e974890cda0542f916be
Aaron Chang <aaronachang@gmail.com>
2023-02-21 19:58:01 -0800

    Fixed reset and parameterized conv_tree_serializer

--- designs/conv_tree_serializer/bb/conv_serializer.v
+++
@@ -1,9 +0,0 @@
-(*blackbox*)
-
-module conv_serializer (CLK,SERIAL_OUT,PAR_IN1,PAR_IN2);
-    input wire CLK;
-    //input RESET;
-    output reg SERIAL_OUT;
-    input wire PAR_IN1,PAR_IN2;
-
-endmodule
